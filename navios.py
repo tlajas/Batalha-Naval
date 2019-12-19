95eb5c4a21bc2c6f216853f569812ba8b5aae5d7
Tiago Lajas <43793354+tlajas@users.noreply.github.com>
2019-12-19 19:02:59 +0000

    Add files via upload
    Criação das matrizes que vão dar origem aos tabuleiros e criado os dicionários dos navios disponiveis de cada jogador

---
+++ navios.py
@@ -0,0 +1,69 @@
+navios_disponiveis_1 = {
+    'tipos_de_navios': [
+          {
+             'nome':'Lancha',
+             'codigo':'L',
+             'tamanho':'1',
+             'quantidade':'4'
+          },
+          {
+             'nome':'Submarino',
+             'codigo':'S',
+             'tamanho':'2',
+             'quantidade':'3'
+          },
+          {
+             'nome':'Fragata',
+             'codigo':'F',
+             'tamanho':'3',
+             'quantidade':'2'
+          },
+          {
+             'nome':'Cruzador',
+             'codigo':'C',
+             'tamanho':'4',
+             'quantidade':'1'
+          },
+          {
+             'nome':'Porta Aviões',
+             'codigo':'P',
+             'tamanho':'5',
+             'quantidade':'1'
+          }
+      ]
+} 
+
+navios_disponiveis_2 = {
+    'tipos_de_navios': [
+          {
+             'nome':'Lancha',
+             'codigo':'L',
+             'tamanho':'1',
+             'quantidade':'4'
+          },
+          {
+             'nome':'Submarino',
+             'codigo':'S',
+             'tamanho':'2',
+             'quantidade':'3'
+          },
+          {
+             'nome':'Fragata',
+             'codigo':'F',
+             'tamanho':'3',
+             'quantidade':'2'
+          },
+          {
+             'nome':'Cruzador',
+             'codigo':'C',
+             'tamanho':'4',
+             'quantidade':'1'
+          },
+          {
+             'nome':'Porta Aviões',
+             'codigo':'P',
+             'tamanho':'5',
+             'quantidade':'1'
+          }
+      ]
+} 
