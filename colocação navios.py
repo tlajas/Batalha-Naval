47d42c71a4ac64fd8657b741409762b314c69424
Tiago Lajas <tiagolajas2000@gmail.com>
2019-12-22 22:46:32 +0000

    Update colocação navios.py

--- colocação navios.py
+++ colocação navios.py
@@ -1,16 +1,27 @@
 import tabuleiro as tab
 import navios
+import jogadores
 
-def colocar_navios(jogador, tipo):
+
+def colocar_navios(jogador, tipo, linha, coluna, orientacao):
     if jogador == jogador_1:
         for navios1 in navios.navios_disponiveis_j1:
             if tipo == navios1['codigo']:
                 navios1['quantidade'] -= 1
+            elif navios2['quantidade'] == 0:
+                print('Não lhe restam mais navios desse tipo para colocar.')
     elif jogador == jogador_2:
         for navios2 in navios.navios_disponiveis_j2:
             if tipo == navios2['codigo']:
                 navios2['quantidade'] -= 1
-    
+            elif navios2['quantidade'] == 0:
+                print('Não lhe restam mais navios desse tipo para colocar.')
 
-for navios1 in navios.navios_disponiveis_j1:
-    print(navios1)    
+
+#def remover_navio(jogador, tipo, linha, coluna, orientacao):
+#    if 
+
+
+
+    
+        
