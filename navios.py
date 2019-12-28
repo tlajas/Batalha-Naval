828ac2ab779f77cc242abe1127364368a5f7563a
Tiago Lajas <tiagolajas2000@gmail.com>
2019-12-28 03:53:40 +0000

    Update navios.py

--- navios.py
+++ navios.py
@@ -3,31 +3,31 @@ navios_disponiveis_j1 = {
           {
              'nome':'Lancha',
              'codigo':'L',
-             'tamanho':'1',
+             'tamanho': 1,
              'quantidade': 4
           },
           {
              'nome':'Submarino',
              'codigo':'S',
-             'tamanho':'2',
+             'tamanho': 2,
              'quantidade': 3
           },
           {
              'nome':'Fragata',
              'codigo':'F',
-             'tamanho':'3',
+             'tamanho': 3,
              'quantidade': 2
           },
           {
              'nome':'Cruzador',
              'codigo':'C',
-             'tamanho':'4',
+             'tamanho': 4,
              'quantidade': 1
           },
           {
              'nome':'Porta Aviões',
              'codigo':'P',
-             'tamanho':'5',
+             'tamanho': 5,
              'quantidade': 1 
           }
       ]
@@ -38,31 +38,31 @@ navios_disponiveis_j2 = {
           {
              'nome':'Lancha',
              'codigo':'L',
-             'tamanho':'1',
+             'tamanho': 1,
              'quantidade': 4
           },
           {
              'nome':'Submarino',
              'codigo':'S',
-             'tamanho':'2',
+             'tamanho': 2,
              'quantidade': 3
           },
           {
              'nome':'Fragata',
              'codigo':'F',
-             'tamanho':'3',
+             'tamanho': 3,
              'quantidade': 2
           },
           {
              'nome':'Cruzador',
              'codigo':'C',
-             'tamanho':'4',
+             'tamanho': 4,
              'quantidade': 1
           },
           {
              'nome':'Porta Aviões',
              'codigo':'P',
-             'tamanho':'5',
+             'tamanho': 5,
              'quantidade': 1
           }
       ]
