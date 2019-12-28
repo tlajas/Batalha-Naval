0289590a3a02ffc7b8d483fdd3dfe9b066dbd906
Tiago Lajas <tiagolajas2000@gmail.com>
2019-12-28 04:56:51 +0000

    Update jogadores.py

--- jogadores.py
+++ jogadores.py
@@ -15,14 +15,21 @@ def tem_jogador(jogo, nome):
         if jogador['nome'] == nome:
             return True
     return False
-    
+   
 
 def remover_jogadores(jogo, nome):
     for jogador in jogo['jogadores']:
-        if jogador['nome'] == nome:
-            jogador['jogadores'].remove(nome)
-            
-def listar_jogador(jogo, nome):
-    jogadores['jogadores'].sort()
-    print (jogadores['jogadores'])
+        jogador['nome'].remove(nome)
+                
+
+def esta_na_lista(jogo, nome):
+    for jogador in jogo['jogadores']:
+        if nome not in jogador['nome']:
+            return True
+    return False
+      
+
+def listar_jogador(jogo):
+    jogo['jogadores'].sort()
+    print (jogo['jogadores'])
     
