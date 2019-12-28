76c4579d92cf7ccd69112263512860620880901e
Tiago Lajas <tiagolajas2000@gmail.com>
2019-12-28 04:26:31 +0000

    Update jogadores.py

--- jogadores.py
+++ jogadores.py
@@ -1,38 +1,27 @@
 def jogadores():
     return {
-        'jogador_1': [],
-        'jogador_2': []
+        'jogadores': []
     } 
 
 def adicionar_jogadores(jogo, nome):
-    jogador_1 = {
+    jogadores = {
         'nome' : nome
     }
-    jogo['jogador_1'].append(jogador_1)
+    jogo['jogadores'].append(jogadores)
 
-    jogador_2 = {
-        'nome' : nome
-    }
-    jogo['jogador_2'].append(jogador_2)
 
 def tem_jogador(jogo, nome):
-    for jogador_1 in jogo['jogador_1']:
-        if jogador_1['nome'] == nome:
+    for jogador in jogo['jogadores']:
+        if jogador['nome'] == nome:
             return True
     return False
-    for jogador_2 in jogo['jogador_2']:
-        if jogador_2['nome'] == nome:
-            return True
-    return False
+    
 
 def remover_jogadores(jogo, nome):
-    for jogador in jogadores['jogadores']:
-        if jogador_1['nome'] == nome:
-            jogadores['jogadores'].remove(jogador1)
-    for jogador in jogadores['jogadores']:
-        if jogador_2['nome'] == nome:
-            jogadores['jogadores'].remove(jogador2)
-
+    for jogador in jogo['jogadores']:
+        if jogador['nome'] == nome:
+            jogador['jogadores'].remove(nome)
+            
 def listar_jogador(jogo, nome):
     jogadores['jogadores'].sort()
     print (jogadores['jogadores'])
