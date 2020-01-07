7fcc994a546628f39e66ab09c53364daae47d096
Tiago Lajas <tiagolajas2000@gmail.com>
2020-01-07 11:19:00 +0000

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -20,7 +20,6 @@ for i in range(10):
     for j in range(10):
         tabuleiro_linha.append(0)
     tabuleiro_ataques_j1.append(tabuleiro_linha)
-print(tabuleiro_ataques_j1)
 
                       
 tabuleiro_ataques_j2 = []
@@ -33,16 +32,16 @@ for i in range(10):
 
 def tabuleiro_navios_j1_colunas(coluna):
     for i in range(0,10):
-        print(tabuleiro_navios_j1[i][coluna])
+        tabuleiro_navios_j1[i][coluna]
 
 def tabuleiro_navios_j2_colunas(coluna):
     for i in range(0,10):
-        print(tabuleiro_navios_j1[i][coluna])
+        tabuleiro_navios_j1[i][coluna]
 
 def tabuleiro_ataques_j1_colunas(coluna):
     for i in range(0,10):
-        print(tabuleiro_navios_j1[i][coluna])
+        tabuleiro_navios_j1[i][coluna]
 
 def tabuleiro_ataques_j2_colunas(coluna):
     for i in range(0,10):
-        print(tabuleiro_navios_j1[i][coluna])
+        tabuleiro_navios_j1[i][coluna]
