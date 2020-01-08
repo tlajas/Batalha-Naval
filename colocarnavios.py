fd13a708abd30c959805f864df1d67dd36212f91
Tiago Lajas <tiagolajas2000@gmail.com>
2020-01-08 01:36:58 +0000

    Create colocarnavios.py

---
+++ colocarnavios.py
@@ -0,0 +1,37 @@
+import tabuleiro as tab
+import navios
+import jogadores
+import main
+
+
+def posicao():
+    pass
+
+def colocar_navios():
+    users = []
+    users = main.obter_jogadores()
+    jogador_1 = users[0]
+    jogador_2 = users[1]
+    print(jogador_1)
+    print(jogador_2)
+    #if jogador == jogador_1:
+    #    for navios1 in navios.navios_disponiveis_j1:
+    #        if tipo == navios1['codigo']:
+    #            navios1['quantidade'] -= 1
+#
+    #        elif navios2['quantidade'] == 0:
+    #            print('Não lhe restam mais navios desse tipo para colocar.')
+    #elif jogador == jogador_2:
+    #    for navios2 in navios.navios_disponiveis_j2:
+    #        if tipo == navios2['codigo']:
+    #            navios2['quantidade'] -= 1
+    #        elif navios2['quantidade'] == 0:
+    #            print('Não lhe restam mais navios desse tipo para colocar.')
+
+
+def coordenadas():
+    pass
+
+
+    
+        
