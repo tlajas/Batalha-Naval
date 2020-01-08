5a0190c91fa46f44c75624a2f50427b66fa84aca
Tiago Lajas <tiagolajas2000@gmail.com>
2020-01-08 01:36:18 +0000

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,47 +1,25 @@
-tabuleiro_navios_j1 = []
+tabuleiro_j1 = []
 for i in range(10):
     tabuleiro_linha = []
     for j in range(10):
         tabuleiro_linha.append(0)
-    tabuleiro_navios_j1.append(tabuleiro_linha)
+    tabuleiro_j1.append(tabuleiro_linha)
 
 
-tabuleiro_navios_j2 = []
+tabuleiro_j2 = []
 for i in range(10):
     tabuleiro_linha = []
     for j in range(10):
         tabuleiro_linha.append(0)
-    tabuleiro_navios_j2.append(tabuleiro_linha)
+    tabuleiro_j2.append(tabuleiro_linha)
 
 
-tabuleiro_ataques_j1 = []
-for i in range(10):
-    tabuleiro_linha = []
-    for j in range(10):
-        tabuleiro_linha.append(0)
-    tabuleiro_ataques_j1.append(tabuleiro_linha)
 
-                      
-tabuleiro_ataques_j2 = []
-for i in range(10):
-    tabuleiro_linha = []
-    for j in range(10):
-        tabuleiro_linha.append(0)
-    tabuleiro_ataques_j2.append(tabuleiro_linha)
-
-
-def tabuleiro_navios_j1_colunas(coluna):
+def tabuleiro_j1_colunas(coluna):
     for i in range(0,10):
-        tabuleiro_navios_j1[i][coluna]
+        tabuleiro_j1[i][coluna] = 1
 
-def tabuleiro_navios_j2_colunas(coluna):
+def tabuleiro_j2_colunas(coluna):
     for i in range(0,10):
-        tabuleiro_navios_j1[i][coluna]
+        tabuleiro_j2[i][coluna] = 1
 
-def tabuleiro_ataques_j1_colunas(coluna):
-    for i in range(0,10):
-        tabuleiro_navios_j1[i][coluna]
-
-def tabuleiro_ataques_j2_colunas(coluna):
-    for i in range(0,10):
-        tabuleiro_navios_j1[i][coluna]
