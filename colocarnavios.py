64fb198ebaa28ec8c3a74c3d619c2464ff42556f
Tiago Lajas <tiagolajas2000@gmail.com>
2020-01-08 23:46:27 +0000

    Update colocarnavios.py

--- colocarnavios.py
+++ colocarnavios.py
@@ -25,9 +25,48 @@ def colocar_navios(jogadores):
     #            print('Não lhe restam mais navios desse tipo para colocar.')
 
 
-def coordenadas():
-    pass
+def coordenadasj1():
+    if comandos[4] == 'A':
+        tab.tabuleiro_j1_colunas(0)
+    elif comandos[4] == 'B':
+        tab.tabuleiro_j1_colunas(1)
+    elif comandos[4] == 'C':
+        tab.tabuleiro_j1_colunas(2)
+    elif comandos[4] == 'D':
+        tab.tabuleiro_j1_colunas(3)
+    elif comandos[4] == 'E':
+        tab.tabuleiro_j1_colunas(4)
+    elif comandos[4] == 'F':
+        tab.tabuleiro_j1_colunas(5)
+    elif comandos[4] == 'G':
+        tab.tabuleiro_j1_colunas(6)
+    elif comandos[4] == 'H':
+        tab.tabuleiro_j1_colunas(7)
+    elif comandos[4] == 'I':
+        tab.tabuleiro_j1_colunas(8)
+    elif comandos[4] == 'J':
+        tab.tabuleiro_j1_colunas(9)
 
 
-    
+def coordenadasj2():
+    if comandos[4] == 'A':
+        tab.tabuleiro_j2_colunas(0)
+    elif comandos[4] == 'B':
+        tab.tabuleiro_j2_colunas(1)
+    elif comandos[4] == 'C':
+        tab.tabuleiro_j2_colunas(2)
+    elif comandos[4] == 'D':
+        tab.tabuleiro_j2_colunas(3)
+    elif comandos[4] == 'E':
+        tab.tabuleiro_j2_colunas(4)
+    elif comandos[4] == 'F':
+        tab.tabuleiro_j2_colunas(5)
+    elif comandos[4] == 'G':
+        tab.tabuleiro_j2_colunas(6)
+    elif comandos[4] == 'H':
+        tab.tabuleiro_j2_colunas(7)
+    elif comandos[4] == 'I':
+        tab.tabuleiro_j2_colunas(8)
+    elif comandos[4] == 'J':
+        tab.tabuleiro_j2_colunas(9)
         
