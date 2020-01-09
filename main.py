763f5304dfe2b8028f4c0a9e7e4a04e273df35cd
Tiago Lajas <tiagolajas2000@gmail.com>
2020-01-09 11:01:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
 import jogadores as jogador
-import tabuleiro as tab
 import colocarnavios as c
+import navios
 
 jogador_1 = ''
 jogador_2 = ''
 
-def main():
-    jogo = jogador.jogadores()
-    nomes = []
-    while True:
-        line = input()
-        if not line:
-            break
-        comandos = line.split(' ')
+tabuleiro_j1 = []
+for i in range(10):
+    tabuleiro_linha = []
+    for j in range(10):
+        tabuleiro_linha.append(0)
+    tabuleiro_j1.append(tabuleiro_linha)
 
-        if comandos[0] == 'RJ':
-            nomes.append(comandosRJ(comandos[1], jogo))
-        elif comandos[0] == 'EJ':
-            comandosEJ(comandos, jogo)
-        elif comandos[0] == 'LJ':
-            comandosLJ(jogo)
-        elif comandos[0] == 'IJ':
-            comandosIJ(comandos, jogo)
-        elif comandos[0] == 'IC':
-            comandosIC(comandos, jogo)
-        elif comandos[0] == 'D':
-            comandosD(comandos, jogo)
-        elif comandos[0] == 'CN':
-            comandosCN(comandos, jogo)
-        elif comandos[0] == 'RN':
-            comandosRN(comandos , jogo)
-        elif comandos[0] == 'T':
-            comandosT(comandos, jogo)
-        elif comandos[0] == 'V':
-            comandosV(comandos, jogo)
-        elif comandos[0] == 'G':
-            comandosG(comandos, jogo)
-        elif comandos[0] == 'L':
-            comandosL(comandosL, jogo)
-        else:
-            print(nomes)
-            print('Instrução inválida')
+
+tabuleiro_j2 = []
+for i in range(10):
+    tabuleiro_linha = []
+    for j in range(10):
+        tabuleiro_linha.append(0)
+    tabuleiro_j2.append(tabuleiro_linha)
+
+
+
+def tabuleiro_j1_colunas(coluna):
+    for i in range(0,10):
+        tabuleiro_j1[i][coluna] = 1
+
+def tabuleiro_j2_colunas(coluna):
+    for i in range(0,10):
+        tabuleiro_j2[i][coluna] = 1
+
+
+
 
 def comandosRJ(nome, jogo):
     nome_jogador = nome
@@ -77,11 +68,91 @@ def comandosIC(comandos, jogo):
 def comandosD(comandos, jogo):
     pass
 
-def comandosCN(comandos, jogo):
-    pass
+def comandosCN(comandos, jogador, tipo, fila, coluna, orientacao):
+    if comandos[1] == jogador_1:
+        for navios1 in navios.navios_disponiveis_j1:
+            if tipo == navios1['codigo']:
+                navios1['quantidade'] -= 1
+            elif navios1['quantidade'] == 0:
+                print('Não lhe restam mais navios desse tipo para colocar.')
+        colunaj1(comandos)    
+        
     
+    else:
+        for navios2 in navios.navios_disponiveis_j2:
+            if tipo == navios2['codigo']:
+                navios2['quantidade'] -= 1
+            elif navios2['quantidade'] == 0:
+                print('Não lhe restam mais navios desse tipo para colocar.')
+        colunaj2(comandos)
+        
 
-    
+
+def orientacao(comandos):
+    if comandos[5] == 'N':
+        colocar_norte()
+    elif comandos[5] == 'S':
+        colocar_sul()
+    elif comandos[5] == 'E':
+        colocar_este()
+    elif comandos[5] == 'O':
+        colocar_oeste()
+
+def colunaj1(comandos):
+    if comandos[4] == 'A':
+        tabuleiro_j1_colunas(0)
+    elif comandos[4] == 'B':
+        tabuleiro_j1_colunas(1)
+    elif comandos[4] == 'C':
+        tabuleiro_j1_colunas(2)
+    elif comandos[4] == 'D':
+        tabuleiro_j1_colunas(3)
+    elif comandos[4] == 'E':
+        tabuleiro_j1_colunas(4)
+    elif comandos[4] == 'F':
+        tabuleiro_j1_colunas(5)
+    elif comandos[4] == 'G':
+        tabuleiro_j1_colunas(6)
+    elif comandos[4] == 'H':
+        tabuleiro_j1_colunas(7)
+    elif comandos[4] == 'I':
+        tabuleiro_j1_colunas(8)
+    elif comandos[4] == 'J':
+        tabuleiro_j1_colunas(9)
+
+def colunaj2(comandos):
+    if comandos[4] == 'A':
+        tabuleiro_j2_colunas(0)
+    elif comandos[4] == 'B':
+        tabuleiro_j2_colunas(1)
+    elif comandos[4] == 'C':
+        tabuleiro_j2_colunas(2)
+    elif comandos[4] == 'D':
+        tabuleiro_j2_colunas(3)
+    elif comandos[4] == 'E':
+        tabuleiro_j2_colunas(4)
+    elif comandos[4] == 'F':
+        tabuleiro_j2_colunas(5)
+    elif comandos[4] == 'G':
+        tabuleiro_j2_colunas(6)
+    elif comandos[4] == 'H':
+        tabuleiro_j2_colunas(7)
+    elif comandos[4] == 'I':
+        tabuleiro_j2_colunas(8)
+    elif comandos[4] == 'J':
+        tabuleiro_j2_colunas(9)
+
+def colocar_norte():
+    pass
+
+def colocar_sul():
+    pass
+
+def colocar_este():
+    pass
+
+def colocar_oeste():
+    pass
         
 def comandosRN(comandos, jogo):
     pass
@@ -94,6 +165,42 @@ def comandosG(comandos, jogo):
 def comandosL(comandos, jogo):
     pass
 
+def main():
+    jogo = jogador.jogadores()
+    nomes = []
+    while True:
+        line = input()
+        if not line:
+            break
+        comandos = line.split(' ')
+
+        if comandos[0] == 'RJ':
+            nomes.append(comandosRJ(comandos[1], jogo))
+        elif comandos[0] == 'EJ':
+            comandosEJ(comandos, jogo)
+        elif comandos[0] == 'LJ':
+            comandosLJ(jogo)
+        elif comandos[0] == 'IJ':
+            comandosIJ(comandos, jogo)
+        elif comandos[0] == 'IC':
+            comandosIC(comandos, jogo)
+        elif comandos[0] == 'D':
+            comandosD(comandos, jogo)
+        elif comandos[0] == 'CN':
+            comandosCN(comandos, jogo, tipo, fila, coluna, orientacao)
+        elif comandos[0] == 'RN':
+            comandosRN(comandos , jogo)
+        elif comandos[0] == 'T':
+            comandosT(comandos, jogo)
+        elif comandos[0] == 'V':
+            comandosV(comandos, jogo)
+        elif comandos[0] == 'G':
+            comandosG(comandos, jogo)
+        elif comandos[0] == 'L':
+            comandosL(comandosL, jogo)
+        else:
+            print(nomes)
+            print('Instrução inválida')
 
 if __name__ == "__main__":
     main()
