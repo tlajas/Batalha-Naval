758ed57f53c86653106105c422391c357970a626
Tiago Lajas <tiagolajas2000@gmail.com>
2020-01-10 10:27:17 +0000

    Update navios.py

--- navios.py
+++ navios.py
@@ -1,34 +1,39 @@
 navios_disponiveis_j1 = {
     'tipos_de_navios': [
           {
-             'nome':'Lancha',
-             'codigo':'L',
-             'tamanho': 1,
-             'quantidade': 4
+             'lancha' : {
+                        'codigo':'L',
+                        'tamanho': 1,
+                        'quantidade': 4
+                        }  
           },
           {
-             'nome':'Submarino',
-             'codigo':'S',
-             'tamanho': 2,
-             'quantidade': 3
+             'submarino':{
+                         'codigo':'S',
+                         'tamanho': 2,
+                         'quantidade': 3
+                         }
           },
           {
-             'nome':'Fragata',
-             'codigo':'F',
-             'tamanho': 3,
-             'quantidade': 2
+             'fragata':{
+                       'codigo':'F',
+                       'tamanho': 3,
+                       'quantidade': 2
+                       }
           },
           {
-             'nome':'Cruzador',
-             'codigo':'C',
-             'tamanho': 4,
-             'quantidade': 1
+             'cruzador':{
+                        'codigo':'C',
+                        'tamanho': 4,
+                        'quantidade': 1
+                        }       
           },
           {
-             'nome':'Porta Aviões',
-             'codigo':'P',
-             'tamanho': 5,
-             'quantidade': 1 
+             'porta avioes':{
+                            'codigo':'P',
+                            'tamanho': 5,
+                            'quantidade': 1 
+                            }
           }
       ]
 } 
@@ -36,34 +41,39 @@ navios_disponiveis_j1 = {
 navios_disponiveis_j2 = {
     'tipos_de_navios': [
           {
-             'nome':'Lancha',
-             'codigo':'L',
-             'tamanho': 1,
-             'quantidade': 4
+             'lancha' : {
+                        'codigo':'L',
+                        'tamanho': 1,
+                        'quantidade': 4
+                        }  
           },
           {
-             'nome':'Submarino',
-             'codigo':'S',
-             'tamanho': 2,
-             'quantidade': 3
+             'submarino':{
+                         'codigo':'S',
+                         'tamanho': 2,
+                         'quantidade': 3
+                         }
           },
           {
-             'nome':'Fragata',
-             'codigo':'F',
-             'tamanho': 3,
-             'quantidade': 2
+             'fragata':{
+                       'codigo':'F',
+                       'tamanho': 3,
+                       'quantidade': 2
+                       }
           },
           {
-             'nome':'Cruzador',
-             'codigo':'C',
-             'tamanho': 4,
-             'quantidade': 1
+             'cruzador':{
+                        'codigo':'C',
+                        'tamanho': 4,
+                        'quantidade': 1
+                        }       
           },
           {
-             'nome':'Porta Aviões',
-             'codigo':'P',
-             'tamanho': 5,
-             'quantidade': 1
+             'porta avioes':{
+                            'codigo':'P',
+                            'tamanho': 5,
+                            'quantidade': 1 
+                            }
           }
       ]
 } 
